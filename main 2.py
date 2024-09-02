dc5faa42bdd746a28619e082ddeea9772745e391
Jose Ricardo Pacheco <pacheco@Pachecos-MacBook-Air.local>
2024-09-01 19:23:24 -0600

    Remove duplicate files main 2.py and requirements 2.txt

--- main 2.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from gui import AuthenticationWindow
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    auth_window = AuthenticationWindow()
-    auth_window.show()
-    sys.exit(app.exec_())
